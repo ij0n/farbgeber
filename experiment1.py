07064428f326ff9ee598ea4a67c95cd527e00622
Kalle Bronsen <bronsen@nrrd.de>
2015-03-13 11:53:57 +0100

    Rename variables more pep8ish.
    Fiddled with `if` expression, so variable will always be defined.

--- experiment1.py
+++ experiment1.py
@@ -9,42 +9,46 @@ from colour import Color
 # zentraler zeitgeber, sollte immer <3600 und >0 sein und integer raustun
 
 
-def generate_html_palette(timevalue=0.0, baseSaturation=1.0, baseLuminance=0.4, huemodifier=0.03, lummodifier=0.07,
-                          satmodifier=0.2, programmcycles=0):
+def generate_html_palette(time_value=0.0, base_saturation=1.0, base_luminance=0.4, hue_modifier=0.03, lum_modifier=0.07,
+                          sat_modifier=0.2, program_cycles=0):
 
     print "zentrale Farbgebeeinheit"
 
-    while programmcycles < 3600:
+    while program_cycles < 3600:
 
-        timevalue = int(strftime("%M", gmtime())) * 60 + int(strftime("%S", gmtime()))
-        timevalue = float(timevalue)
+        time_value = int(strftime("%M", gmtime())) * 60 + int(strftime("%S", gmtime()))
+        time_value = float(time_value)
 
-        baseHue = timevalue / 3600
-        baseColor = Color(hsl=(baseHue, baseSaturation, baseLuminance))
+        base_hue = time_value / 3600
+        base_color = Color(hsl=(base_hue, base_saturation, base_luminance))
 
-        baseColorVar1 = Color(hsl=(baseColor.hue + huemodifier, baseSaturation - satmodifier, baseLuminance))
-        baseColorVar2 = Color(hsl=(baseColor.hue - huemodifier, baseSaturation - satmodifier, baseLuminance))
-        baseColorVar3 = Color(hsl=(baseColor.hue, baseSaturation, baseLuminance + lummodifier))
-        baseColorVar4 = Color(hsl=(baseColor.hue, baseSaturation, baseLuminance - lummodifier))
+        base_color_variant_1 = Color(
+            hsl=(base_color.hue + hue_modifier, base_saturation - sat_modifier, base_luminance))
+        base_color_variant_2 = Color(
+            hsl=(base_color.hue - hue_modifier, base_saturation - sat_modifier, base_luminance))
+        base_color_variant_3 = Color(
+            hsl=(base_color.hue, base_saturation, base_luminance + lum_modifier))
+        base_color_variant_4 = Color(
+            hsl=(base_color.hue, base_saturation, base_luminance - lum_modifier))
 
-        base_degree = baseHue * 360
+        base_degree = base_hue * 360
         if base_degree < 180:
-            ContrastHue = base_degree + 180
-        elif base_degree >= 180:
-            ContrastHue = base_degree - 180
-        ContrastHue = ContrastHue / 360
+            contrast_hue = base_degree + 180
+        else:
+            contrast_hue = base_degree - 180
+        contrast_hue /= 360
 
-        ContrastColor = Color(hsl=(ContrastHue, baseSaturation - satmodifier, (baseLuminance + 0.2)))
+        contrast_color = Color(hsl=(contrast_hue, base_saturation - sat_modifier, (base_luminance + 0.2)))
 
         # Terminaloutput
 
-        # print timevalue
-        # print "baseColor ", baseColor.hex
-        # print "baseColorVariant1 ", baseColorVar1
-        # print "baseColorVariant2 ", baseColorVar2
-        # print "baseColorVariant3 ", baseColorVar3
-        # print "baseColorVariant4 ", baseColorVar4
-        # print "ContrastColor ", ContrastColor.hex
+        # print time_value
+        # print "base_color ", base_color.hex
+        # print "baseColorVariant1 ", base_color_variant_1
+        # print "baseColorVariant2 ", base_color_variant_2
+        # print "baseColorVariant3 ", base_color_variant_3
+        # print "baseColorVariant4 ", base_color_variant_4
+        # print "contrast_color ", contrast_color.hex
         # print "###################################"
 
         # Weboutput
@@ -53,21 +57,21 @@ def generate_html_palette(timevalue=0.0, baseSaturation=1.0, baseLuminance=0.4, 
 <style type="text/css">
 """
         htmlcontent = """</style></head><body><h1>visuelle Ausgabeeinheit des zentralen Farbgebers</h1>
-<div>BaseColor """ + baseColor.hex + """</div></ br>
-<div class="baseColorVar1">baseColorVariant1 """ + baseColorVar1.hex + """</div>
-<div class="baseColorVar2">baseColorVariant2 """ + baseColorVar2.hex + """</div>
-<div class="baseColorVar3">baseColorVariant3 """ + baseColorVar3.hex + """</div>
-<div class="baseColorVar4">baseColorVariant4 """ + baseColorVar4.hex + """</div>
-<div class="Contrastcolor">Contrastcolor """ + ContrastColor.hex + """</div>"""
-        zeitzeile = "<h3>Color-Seed " + str(timevalue) + " " + strftime("%H:%M:%S", gmtime()) + "Uhr</h3>"
+<div>BaseColor """ + base_color.hex + """</div></ br>
+<div class="base_color_variant_1">baseColorVariant1 """ + base_color_variant_1.hex + """</div>
+<div class="base_color_variant_2">baseColorVariant2 """ + base_color_variant_2.hex + """</div>
+<div class="base_color_variant_3">baseColorVariant3 """ + base_color_variant_3.hex + """</div>
+<div class="base_color_variant_4">baseColorVariant4 """ + base_color_variant_4.hex + """</div>
+<div class="Contrastcolor">Contrastcolor """ + contrast_color.hex + """</div>"""
+        zeitzeile = "<h3>Color-Seed " + str(time_value) + " " + strftime("%H:%M:%S", gmtime()) + "Uhr</h3>"
         htmlclosing = """</body></html>"""
 
-        css1 = "body { font-size:20px; background-color:" + baseColor.hex + "; color:" + ContrastColor.hex + "; }"
-        css2 = ".baseColorVar1 { background-color:" + baseColorVar1.hex + "; width:100%; height:40px; padding: 40px; font-size:20px; } \n\r"
-        css3 = ".baseColorVar2 { background-color:" + baseColorVar2.hex + "; width:50%; height:40px; padding: 40px; font-size:20px; } \n\r"
-        css4 = ".baseColorVar3 { background-color:" + baseColorVar3.hex + "; width:100%; height:40px; padding: 40px; font-size:20px; } \n\r"
-        css5 = ".baseColorVar4 { background-color:" + baseColorVar4.hex + "; width:50%; height:40px; padding: 40px; font-size:20px; } \n\r"
-        css6 = ".Contrastcolor { background-color:" + ContrastColor.hex + "; width:10%; height:900px; position:absolute; right:300px; top:0px; color:" + baseColor.hex + "; padding: 40px; font-size:20px; } \n"
+        css1 = "body { font-size:20px; background-color:" + base_color.hex + "; color:" + contrast_color.hex + "; }"
+        css2 = ".base_color_variant_1 { background-color:" + base_color_variant_1.hex + "; width:100%; height:40px; padding: 40px; font-size:20px; } \n\r"
+        css3 = ".base_color_variant_2 { background-color:" + base_color_variant_2.hex + "; width:50%; height:40px; padding: 40px; font-size:20px; } \n\r"
+        css4 = ".base_color_variant_3 { background-color:" + base_color_variant_3.hex + "; width:100%; height:40px; padding: 40px; font-size:20px; } \n\r"
+        css5 = ".base_color_variant_4 { background-color:" + base_color_variant_4.hex + "; width:50%; height:40px; padding: 40px; font-size:20px; } \n\r"
+        css6 = ".Contrastcolor { background-color:" + contrast_color.hex + "; width:10%; height:900px; position:absolute; right:300px; top:0px; color:" + base_color.hex + "; padding: 40px; font-size:20px; } \n"
 
         f = open('output1.html', 'w')
         outputtxt = str(htmlpreface) + str(css1) + str(css2) + str(css3) + str(css4) + str(css5) + str(css6) + str(
@@ -75,7 +79,7 @@ def generate_html_palette(timevalue=0.0, baseSaturation=1.0, baseLuminance=0.4, 
         f.write(outputtxt)
         f.close()
 
-        programmcycles += 1
+        program_cycles += 1
         time.sleep(1)
 
 if __name__ == "__main__":
